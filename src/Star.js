1eb58bfc271fdde7515cc096b6e6154cf28895ed
Nico Rithner <nicorithner@gmail.com>
2022-10-11 20:55:13 -0600

    Add change color on select

--- src/Star.js
+++ src/Star.js
@@ -1,5 +1,5 @@
 import { FaStar } from "react-icons/fa";
 
-export const Star = ({ selected = false }) => {
-  return <FaStar color={selected ? "red" : "gray"} />;
+export const Star = ({ selected = false, onSelect }) => {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
 };
--- src/StarRating.js
+++ src/StarRating.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { Star } from "./Star";
 
 const createArray = (length) => [...Array(length)];
 
-const StarRating = ({totalStars = 5}) => {
-  return createArray(totalStars).map((_n, i) => <Star key={i} />);
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((_n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        Rating: {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
 };
 
 export default StarRating;
