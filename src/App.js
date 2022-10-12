46a1b3026266590fb36b8fdb94c1ca872e0fa6b8
Nico Rithner <nicorithner@gmail.com>
2022-10-11 20:43:21 -0600

    Update App.js to show starts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { useState } from "react";
 import "./App.css";
+import StarRating from "./StarRating";
 
 function App() {
-  const [checked, setChecked] = useState(false);
-  return (
-    <div>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
-  );
+  return <StarRating totalStars={10} />;
 }
 
 export default App;
