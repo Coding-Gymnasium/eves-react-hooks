9540090f7a58832f0373c099f08ab3da0eec6e66
Nico Rithner <nicorithner@gmail.com>
2022-10-11 20:12:54 -0600

    Practice useState basics

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// const people = ["Alex", "Ali", "Anna"];
-// const [first, second, third] = ["Alex", "Ali", "Anna"];
-// const [first] = ["Alex", "Ali", "Anna"];
-const [, , third] = ["Alex", "Ali", "Anna"];
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App name={third} />
+    <App />
   </React.StrictMode>
 );
