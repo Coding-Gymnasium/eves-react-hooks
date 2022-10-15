9f09e6e6ebf14459a33e8031c77ac2eea373da96
Nico Rithner <nicorithner@gmail.com>
2022-10-15 00:10:53 -0600

    Add dependency arrays

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("Jan");
+  const [admin, setAdmin] = useState(false);
 
   useEffect(() => {
-    document.title = `Celebrate ${name}`;
-  }, []);
+    console.log(`Celebrate ${name}`);
+  }, [name]);
+
+  useEffect(() => {
+    console.log(`The user is: ${admin ? "Admin" : "Not Admin"}`);
+  }, [admin]);
+
   return (
     <section>
       <p>Congratulations {name}!</p>
       <button onClick={() => setName("Will")}>Change Winner</button>
+      <p>{admin ? "logged in" : "not logged in"}</p>
+      <button onClick={() => setAdmin(true)}>Log In</button>
     </section>
   );
 }
