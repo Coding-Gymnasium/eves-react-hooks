b810061cdf4efd09a2d1756f55c601e08f6dff8d
Nico Rithner <nicorithner@gmail.com>
2022-10-21 17:06:09 -0600

    First useReducer exercise

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const getUsers = () => {
-    fetch("https://api.github.com/users")
-      .then((response) => response.json())
-      .then(setData);
-  };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  return (
-    <>
-      <div>
-        <button onClick={() => setData([])}>Remove Data</button>
-        <button onClick={getUsers}>Get Data</button>
-      </div>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.login}</li>
-        ))}
-      </ul>
-    </>
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber,
+    0
   );
+  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 }
 
 export default App;
