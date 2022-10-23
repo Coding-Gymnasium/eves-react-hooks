0b7bb02287b64cb5dce9adae86df5cd665e0c899
Nico Rithner <nicorithner@gmail.com>
2022-10-23 15:44:11 -0600

    Creat context

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
+import { useContext } from "react";
+import { TreesContext } from "./";
 import "./App.css";
-import { useInput } from "./useInput";
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
-
-  const submit = (e) => {
-    e.preventDefault();
-    const titleVal = titleProps.value;
-    const colorVal = colorProps.value;
-    console.log(`${titleVal} sounds like ${colorVal}`);
-    resetTitle("");
-    resetColor("#FFFFFF");
-  };
+  const { trees } = useContext(TreesContext);
 
   return (
     <>
-      <form onSubmit={submit}>
-        <input
-          {...titleProps}
-          type="text"
-          placeholder="title..."
-        />
-        <input
-          {...colorProps}
-          type="color"
-        />
-        <button>ADD</button>
-      </form>
+      <h1>Trees</h1>
+      <ul>
+        {trees.map((tree) => (
+          <li key={tree.id}>{tree.type}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+export const TreesContext = createContext();
+
+const trees = [
+  { id: "1", type: "Maple" },
+  { id: "2", type: "Oak" },
+  { id: "3", type: "Family" },
+  { id: "4", type: "Components" },
+];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <TreesContext.Provider value={{ trees }}>
+    <App />
+  </TreesContext.Provider>
+);
